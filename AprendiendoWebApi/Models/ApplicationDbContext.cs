c56c6a305fc2f5b840f9d9d836e35dbeead4778d
Adrian Muñoz Mudarra <eladrixhd@gmail.com>
2018-08-09 18:59:42 +0200

    Aprendiendo ASP #2 (WebApi)

---
+++ AprendiendoWebApi/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AprendiendoWebApi.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Pais> Paises { get; set; }
+        public DbSet<Provincia> Provincias { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+            
+        }
+    }
+}
--- AprendiendoWebApi/Startup.cs
+++ AprendiendoWebApi/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AprendiendoWebApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,11 +27,17 @@ namespace AprendiendoWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<ApplicationDbContext>(option => option.UseInMemoryDatabase("paisDB"));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddJsonOptions(ConfigureJson);
+        }
+
+        private void ConfigureJson(MvcJsonOptions obj)
+        {
+            obj.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext context)
         {
             if (env.IsDevelopment())
             {
@@ -42,6 +50,26 @@ namespace AprendiendoWebApi
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            if (!context.Paises.Any())
+            {
+                context.Paises.AddRange(new List<Pais>()
+                {
+                    new Pais(){Nombre = "España", Provincias = new List<Provincia>(){
+                            new Provincia(){Nombre = "Granada"}
+                    } },
+                    new Pais()
+                    { Nombre = "Francia", Provincias = new List<Provincia>()
+                        {
+                                new Provincia(){Nombre = "Paris"},
+                                new Provincia(){Nombre = "Niza"}
+                        }
+                    },
+                    new Pais(){Nombre = "Alemania"}
+                });
+
+                context.SaveChanges();
+            }
         }
     }
 }
